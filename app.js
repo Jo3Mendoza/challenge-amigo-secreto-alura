8a26b19dc3a2d6bda9cca0013707075552547b2e
Jo3Mendoza <onaiwin@gmail.com>
2025-08-11 14:28:34 -0500

    Adicion de funcion mostrarLista

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres
+let amigos = []; 
+
 function agregarAmigo(){
     const input = document.getElementById("amigo");
     const nombre = input.value.trim();
 
     if (nombre) {
-        const lista = document.getElementById("listaAmigos");
-        const li = document.createElement("li");
-        li.textContent = nombre;
-        lista.appendChild(li);
-
+        amigos.push(nombre); // Guardar en el array
+        mostrarLista(); // Actualizar la lista en HTML
         input.value = "";
         input.focus();
     } else {
-        alert("Por favor, ingrese un nombre valido.");
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function mostrarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar lista antes de agregar nuevos elementos
+    for (let amigo of amigos) {
+        lista.innerHTML += `<li>${amigo}</li>`;
     }
 }
